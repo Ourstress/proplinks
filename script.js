38d6eca1234b419f7c602b62999f94bb55640241
SG <sgng1@e.ntu.edu.sg>
2018-09-23 15:52:21 +0100

    added feature to display only one content div at any one time

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let navArray = [["nav1","#process1"],["nav2","#process2"],["nav3","#process3"]]
 let navIDs = new Map(navArray)
 
 document.addEventListener("DOMContentLoaded", function(){
+    let allNavs = document.querySelectorAll(".navDiv")
     let processNav = document.querySelector("#processNav")
     processNav.addEventListener("click", function(event){
+        allNavs.forEach(function(element){
+            element.classList.contains("invisible")? "" : element.classList.toggle("invisible")
+        })
         let selection = document.querySelector(navIDs.get(event.target.id))
-        selection.classList.toggle("visible")
+        selection.classList.toggle("invisible")
     })
 }) 
